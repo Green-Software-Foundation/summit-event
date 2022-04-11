91645ce27f4170c32c6facbcee69386aaeae1561
Osama Jandali <osamajandaly@gmail.com>
2022-04-11 05:15:58 +0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,5 +86,11 @@ module.exports = {
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-593HJXNS18"],
+      },
+    },
   ],
-};
+}
